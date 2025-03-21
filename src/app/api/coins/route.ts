3ae48b085347474087daa53254e41580198b21db
Edib Esad Galip <edibesad@email.com>
2025-03-21 11:07:17 +0300

    Enhance API response to include search functionality and total count of USDT symbols

--- src/app/api/coins/route.ts
+++ src/app/api/coins/route.ts
@@ -2,11 +2,16 @@ export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const limit = searchParams.get("limit");
   const offset = searchParams.get("offset");
+  const search = searchParams.get("search");
   const data = await (
     await fetch("https://api.binance.com/api/v3/ticker/24hr")
   ).json();
+  console.log("search", search);
 
   const symbols = data
+    .filter((symbol: any) =>
+      search ? symbol.symbol.includes(search.toUpperCase()) : true
+    )
     .filter((symbol: any) => symbol.symbol.endsWith("USDT"))
     .slice(Number(offset), Number(offset) + Number(limit))
     .map((symbol: any) => ({
@@ -16,10 +21,21 @@ export async function GET(request: Request) {
       volume: symbol.volume,
       lastPrice: symbol.lastPrice,
     }));
+  console.log("symbols", symbols);
 
-  return new Response(JSON.stringify(symbols), {
-    headers: {
-      "content-type": "application/json",
-    },
-  });
+  return new Response(
+    JSON.stringify({
+      data: symbols,
+      total: data
+        .filter((symbol: any) => symbol.symbol.endsWith("USDT"))
+        .filter((symbol: any) =>
+          search ? symbol.symbol.includes(search.toUpperCase()) : true
+        ).length,
+    }),
+    {
+      headers: {
+        "content-type": "application/json",
+      },
+    }
+  );
 }
